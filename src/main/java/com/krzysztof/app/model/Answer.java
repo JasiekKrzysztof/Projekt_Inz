00fdb651d60ef8a8a494990eff228439ad5a79d4
Jan Krzysztof <jan.krzysztof1997@gmail.com>
2020-04-29 13:37:39 +0200

    add new table in database

--- src/main/java/com/krzysztof/app/model/Answer.java
+++ src/main/java/com/krzysztof/app/model/Answer.java
@@ -1,9 +1,6 @@
 package com.krzysztof.app.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Answer {
@@ -12,6 +9,124 @@ public class Answer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long idAnswer;
 
-    
+    @OneToOne
+    private Questionnaire idQuestionnaire;
 
+    @OneToOne
+    private Users idUser;
+
+    String answer1;
+    String answer2;
+    String answer3;
+    String answer4;
+    String answer5;
+    String answer6;
+    String answer7;
+    String answer8;
+    String answer9;
+    String answer10;
+
+    public Long getIdAnswer() {
+        return idAnswer;
+    }
+
+    public void setIdAnswer(Long idAnswer) {
+        this.idAnswer = idAnswer;
+    }
+
+    public Questionnaire getIdQuestionnaire() {
+        return idQuestionnaire;
+    }
+
+    public void setIdQuestionnaire(Questionnaire idQuestionnaire) {
+        this.idQuestionnaire = idQuestionnaire;
+    }
+
+    public Users getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Users idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public void setAnswer1(String answer1) {
+        this.answer1 = answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public void setAnswer2(String answer2) {
+        this.answer2 = answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public void setAnswer3(String answer3) {
+        this.answer3 = answer3;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public void setAnswer4(String answer4) {
+        this.answer4 = answer4;
+    }
+
+    public String getAnswer5() {
+        return answer5;
+    }
+
+    public void setAnswer5(String answer5) {
+        this.answer5 = answer5;
+    }
+
+    public String getAnswer6() {
+        return answer6;
+    }
+
+    public void setAnswer6(String answer6) {
+        this.answer6 = answer6;
+    }
+
+    public String getAnswer7() {
+        return answer7;
+    }
+
+    public void setAnswer7(String answer7) {
+        this.answer7 = answer7;
+    }
+
+    public String getAnswer8() {
+        return answer8;
+    }
+
+    public void setAnswer8(String answer8) {
+        this.answer8 = answer8;
+    }
+
+    public String getAnswer9() {
+        return answer9;
+    }
+
+    public void setAnswer9(String answer9) {
+        this.answer9 = answer9;
+    }
+
+    public String getAnswer10() {
+        return answer10;
+    }
+
+    public void setAnswer10(String answer10) {
+        this.answer10 = answer10;
+    }
 }
